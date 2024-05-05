d2d334fbd79ae3f46c59f4d2d94ec3165ae84e04
Mikk155 <martimsanchez155@gmail.com>
2024-05-05 18:33:54 -0300

    Python FGD Generator by json decoding

--- changelog.md
+++ changelog.md
@@ -15,7 +15,179 @@ This file will be updated from above to bellow, meaning besides from this text, 
 <details><summary>2024</summary><p>
 
 <!-- Start of May -->
-<details><summary>4 (May)</summary><p>
+<details><summary>5 (May)</summary><p>
+
+<details><summary>05</summary><p>
+
+- Created a [script](scripts/forge_game_data.py) for building a FGD file from json information
+
+- Created [BaseClass.json](src/fgd/en/EntityData/BaseClass.json) containing all the @BaseClass
+
+- Created a script for porting FGD to json formats that we're going to use later on [forge_game_data.py](scripts/forge_game_data.py)
+
+<details><summary>Script in case of re-usage</summary><p>
+
+```python
+import json, os
+
+with open( 'halflife-unified.fgd', 'r') as fgd:
+    lineas = fgd.readlines()
+
+ReadingEnt = False
+InChoices = False
+InSpawnflags = False
+
+datos_json = {}
+KeyValueField = {}
+Choices = {}
+
+for line in lineas:
+
+    if line.startswith( '@' ):
+        ReadingEnt = True
+        ClassData = {}
+
+        Class = line[ 1: line.find( 'Class' ) ]
+
+        if Class == 'Point':
+            ClassData[ 'type' ] = 'Point'
+
+        Bases = [ 'Mandatory' ]
+        if line.find( 'base(' ) != -1:
+            bases = line[ line.find( 'base(' ) + 5: line.find( ')', line.find( 'base(' ) ) ].split( ',' )
+            for base in bases:
+                if base != 'Targetname' and base != 'Appearflags':
+                    base = base.strip()
+                    Bases.append( base )
+        Bases.append( 'Appearflags' )
+
+        if line.find( 'size(' ) != -1:
+            Size = []
+            size = line[ line.find( 'size(' ) + 5: line.find( ')', line.find( 'size(' ) + 5 ) ].split( ',' )
+            for siz in size:
+                Size.append( siz.split() )
+            ClassData[ 'size' ] = Size
+
+        if line.find( 'color(' ) != -1:
+            Color = line[ line.find( 'color(' ) + 6: line.find( ')', line.find( 'color(' ) + 6) ].split( ' ' )
+            ClassData[ 'color' ] = Color
+
+        if line.find( 'studio(' ) != -1:
+            Studio = line[ line.find( 'studio(' ) + 7: line.find( ')', line.find( 'studio(' ) + 7 ) ]
+            ClassData[ 'studio' ] = Studio.strip('"')
+
+        if line.find( 'sprite(' ) != -1:
+            Sprite = line[ line.find( 'sprite(' ) + 7: line.find( ')', line.find( 'sprite(' ) + 7 ) ]
+            ClassData[ 'sprite' ] = Sprite.strip('"')
+
+        if line.find( 'flags(' ) != -1:
+            flags = line[ line.find( 'flags(' ) + 6: line.find( ')', line.find( 'flags(' ) + 6 ) ]
+            ClassData[ 'flags' ] = flags.split(',')
+
+        ClassData[ 'base' ] = Bases
+
+        if line.find( '= ' ) != -1:
+            ClassName = line[ line.find( '= ' ) + 2: line.find( ':', line.find( '= ' ) + 3 ) ].strip()
+            ClassData[ 'classname' ] = ClassName
+
+        if line.find( ': "' ) != -1:
+            Name = line[ line.find( ': "' ) + 3: line.find( '"', line.find( ': "' ) + 3 ) ]
+            ClassData[ 'name' ] = Name
+
+        ClassData[ 'description' ] = ''
+
+        datos_json[ 'ClassData' ] = ClassData
+
+    elif InSpawnflags:
+        dline = line.strip()
+        if dline and dline != '' and dline != '[':
+
+            if dline[0] == ']':
+                KeyValueField = Choices
+                datos_json[ Key ] = KeyValueField
+                KeyValueField = {}
+                Choices = {}
+                InSpawnflags = False
+            else:
+                ToChoices = {
+                    "name": dline[ dline.find( ':' ) + 1 : dline.rfind( '"' ) ].strip().strip( '"' ),
+                    "description": ""
+                }
+                ToAllChoices = Choices.get( 'spawnflags', {} )
+                ToAllChoices[ dline[ : dline.find( ':' ) ].strip() ] = ToChoices
+                Choices= ToAllChoices
+
+    elif InChoices:
+        dline = line.strip()
+        if dline and dline != '' and dline != '[':
+
+            if dline[0] == ']':
+                KeyValueField= Choices
+                datos_json[ Key ] = KeyValueField
+                KeyValueField = {}
+                Choices = {}
+                InChoices = False
+            else:
+                ToChoices = {
+                    "name": dline[ dline.find( ':' ) + 1 : ].strip().strip( '"' ),
+                    "description": ""
+                }
+                ToAllChoices = Choices.get( 'choices', {} )
+                ToAllChoices[ dline[ : dline.find( ':' ) ].strip() ] = ToChoices
+                Choices[ 'choices' ] = ToAllChoices
+
+    elif ReadingEnt and line[0] == ']':
+        path = os.path.abspath( datos_json.get( 'ClassData', {} )[ 'classname' ] + '.json' )
+        with open( path, 'w') as js:
+            js.write(json.dumps(datos_json, indent=4))
+        datos_json = {}
+        ReadingEnt = False
+    else:
+        dline = line.strip()
+
+        if dline.find( '(' ) != -1:
+            Key = dline[ 0 : dline.find( '(' ) ]
+            if Key == 'spawnflags':
+                InSpawnflags = True
+                Choices = KeyValueField
+            elif Key and Key != '':
+
+                Type = dline[ dline.find( '(' ) + 1 : dline.find( ')' ) ].lower().strip()
+                KeyValueField[ 'type' ] = Type
+
+                Name = dline[ dline.find( '"' ) + 1 : dline.find( '"', dline.find( '"' ) + 1 ) ]
+                KeyValueField[ 'name' ] = Name
+                KeyValueField[ 'description' ] = ''
+
+                Value = ''
+                if Type == 'choices':
+                    Value = dline[ dline.find( ':', dline.find( '"' ) ) + 1 : dline.find( '=' ) ].strip().strip( '"' )
+                    InChoices = True
+                    KeyValueField[ 'value' ] = Value
+                    Choices = KeyValueField
+                elif dline.find( ':', dline.find( '"' ) ) != -1:
+                    Value = dline[ dline.find( ':', dline.find( '"' ) ) + 1 :  ].strip().strip( '"' )
+                    KeyValueField[ 'value' ] = Value
+                    datos_json[ Key ] = KeyValueField
+                    KeyValueField = {}
+    ClassData = {}
+```
+
+</p></details>
+
+---
+
+</p></details>
+
+<details><summary>04</summary><p>
+
+- Disable "Wiki" on the repository
+
+- Enable "Issues" on the repository
+
+---
+
+</p></details>
 
 <details><summary>01</summary><p>
 
@@ -31,13 +203,13 @@ This file will be updated from above to bellow, meaning besides from this text, 
 </p></details>
 
 <!-- Start of April -->
-<details><summary>3 (April)</summary><p>
+<details><summary>4 (April)</summary><p>
 
 <details><summary>30</summary><p>
 
 - Added base web site to this repository
 
-- Created a [script](src/build.py) for building it by the use of multiple languages
+- Created a [script](scripts/build_website.py) for building website by the use of multiple languages
 
 - Create a workflow rules to auto release compiled binaries upon a commit to src/
 
---
+++ scripts/forge_game_data.py
@@ -0,0 +1,224 @@
+# This Python script builds our FGD file and our website's Entity Guide page.
+
+import os, json
+
+DEBUG = False
+
+abs = os.path.abspath( '../' )
+
+def WriteStudio( ClassData ):
+    std = [ 'studio', 'sprite' ]
+    for Studio in std:
+        if Studio in ClassData:
+            studio = ClassData.get( Studio )
+            FGD.write( f'{Studio}(')
+            if isinstance( studio, bool ):
+                FGD.write( ') ')
+            else:
+                FGD.write( f' "{studio}" ) ')
+
+def WriteIconSprite( ClassData ):
+    if 'iconsprite' in ClassData:
+        iconsprite = ClassData.get( 'iconsprite' )
+        FGD.write( f'iconsprite( {iconsprite} ) ')
+
+def WriteOffSet( ClassData ):
+    if 'offset' in ClassData:
+        offset = ClassData.get( 'offset' )
+        FGD.write( f'offset( ')
+        for i in offset:
+            FGD.write( f'{i} ')
+        FGD.write( f') ')
+
+
+def WriteColor( ClassData ):
+    if 'color' in ClassData:
+        FGD.write( 'color( ')
+        for rgb in ClassData.get( 'color', [] ):
+            FGD.write(f'{rgb} ')
+        FGD.write( ') ')
+
+def WriteSize( ClassData ):
+    if 'size' in ClassData:
+        FGD.write( 'size(')
+        for i, size in enumerate( ClassData.get( 'size', [] ) ):
+            for m in size:
+                FGD.write(f' {m}')
+            if i == 0:
+                FGD.write( ',' )
+        FGD.write( ' ) ')
+
+def WriteFlags( ClassData ):
+    if 'flags' in ClassData:
+        FGD.write( 'flags(')
+        for i, flags in enumerate( ClassData.get( 'flags', [] ) ):
+            FGD.write(f' {flags}')
+            if i < len( ClassData.get( 'flags', [] ) ) - 1:
+                FGD.write( ',' )
+        FGD.write( ' ) ')
+
+def WriteBase( ClassData, IsEntity = False ):
+    if 'BaseClass' in ClassData:
+        if IsEntity:
+            FGD.write( f', ' )
+        else:
+            FGD.write( f'base( ' )
+        base = ClassData.get( 'BaseClass', [] )
+        for i, b in enumerate(base):
+            FGD.write( f'{b}' )
+            if i < len(base) - 1:
+                FGD.write( ', ' )
+        FGD.write( ' ) ' )
+    elif IsEntity:
+        FGD.write( ' ) ' )
+
+def WriteSpawnflags( js ):
+    if 'spawnflags' in js:
+        FGD.write( f'\tspawnflags(Flags) =\n\t[\n' )
+        for sk, sv in js.get( 'spawnflags', {} ).items():
+            svname = sv.get( 'name', '' )
+            svdescription = sv.get( 'description', '' )
+            i = int(sk)
+            if ( i & ( i - 1 ) ) != 0:
+                print( f'Error! Wrong key value bit set, please fix flag "{svname}" bit "{sk}"')
+                exit(1)
+            FGD.write( f'\t\t{sk} : "{svname}" : ' )
+            if 'active' in sv and sv.get( 'active', False ):
+                FGD.write( '1' )
+            else:
+                FGD.write( '0' )
+            FGD.write( f' : "{svdescription}"\n' )
+        FGD.write('\t]\n')
+
+def WriteEntity( TypeClass, js, BaseClassName = '' ):
+    ClassData = js.get( 'ClassData', {} )
+    name = ClassData.get( 'name', '' )
+    classname = ClassData.get( 'classname', '' )
+    description = ClassData.get( 'description', '' )
+
+    if TypeClass == 'Base' or classname == 'worldspawn' or classname == 'infodecal':
+
+        FGD.write( f'@{TypeClass}Class ' )
+        WriteColor( ClassData )
+        WriteSize( ClassData )
+        WriteFlags( ClassData )
+        WriteOffSet( ClassData )
+        WriteStudio( ClassData )
+        WriteIconSprite( ClassData )
+
+        if classname == 'infodecal':
+            FGD.write( f'decal() ' )
+
+        if BaseClassName != '':
+            WriteBase( ClassData )
+
+        FGD.write( f'= ' )
+
+        if TypeClass == 'Base' and BaseClassName == '':
+            FGD.write( 'C' )
+
+        if BaseClassName == '':
+            FGD.write( f'{classname} : "{name}" : "{description}"\n[\n' )
+        else:
+            FGD.write( f'{BaseClassName}\n[\n' )
+
+        WriteSpawnflags( js )
+
+        if classname == 'infodecal':
+            FGD.write( f'\ttexture(decal)\n' )
+
+        for key, values in js.items():
+
+            if key == 'choices' or key == 'ClassData' or key == 'spawnflags':
+                continue
+
+            type = values.get( 'type', '' )
+            name = values.get( 'name', '' )
+            value = values.get( 'value', '' )
+            description = values.get( 'description', '' )
+
+            # Hack hack, https://developer.valvesoftware.com/wiki/FGD says Hammer die reading quoted integers
+            #if type != 'integer':
+            value = f'"{value}"'
+            # Undone, -Mikk ask in program wich program will you use the FGD for.
+            # Hammer will also die if integer is empty ( "name" : : "description" )
+            # Meant intentionaly empty (not 0) for specific keyvalues that shouldn't get handled to KeyValue bool.
+
+            FGD.write( f'\t{key}({type}) : "{name}" : {value} : "{description}"' )
+            if 'choices' in values:
+                FGD.write( ' =\n' )
+                choices = values.get( 'choices', {} )
+                FGD.write( f'\t[\n' )
+                for ck, cv in choices.items():
+                    cn = cv.get( 'name', '' )
+                    cd = cv.get( 'description', '' )
+                    FGD.write( f'\t\t"{ck}" : "{cn}" : "{cd}"\n' )
+                FGD.write( f'\t]\n' )
+            else:
+                FGD.write( '\n' )
+
+        FGD.write( f']\n\n' )
+        return
+
+    if len(js) > 1:
+        if BaseClassName != 'IsCopyPoint':
+            WriteEntity( 'Base', js )
+            if DEBUG:
+                print(f'{classname}')
+
+    FGD.write( f'@{TypeClass}Class ')
+    #WriteColor( ClassData )
+    #WriteSize( ClassData )
+    #WriteFlags( ClassData )
+    #WriteOffSet( ClassData )
+    #WriteStudio( ClassData )
+    #WriteIconSprite( ClassData )
+
+    if len(js) > 1:
+        FGD.write( f'base( C{classname}' )
+        if BaseClassName == 'IsCopyPoint':
+            FGD.write( f', hullsizes' )
+        WriteBase( ClassData, True )
+    else:
+        WriteBase( ClassData )
+
+    FGD.write( f' = {classname} : "{name}" : "{description}" []\n\n' )
+
+def ReadData( path ):
+    with open( path, 'r' ) as f:
+        js = json.load( f )
+        f.close()
+
+    if js:
+        data = js.get( 'ClassData', {} )
+        if path.endswith( 'BaseClass.json' ):
+            for k, v in js.items():
+                WriteEntity( 'Base', v, k )
+        elif 'type' in data:
+            WriteEntity( data.get( 'type' , '' ), js )
+        else:
+            WriteEntity( 'Solid', js )
+            WriteEntity( 'Point', js, 'IsCopyPoint' )
+
+
+LangFolders = os.listdir( os.path.abspath( f'{abs}/src/fgd/' ) )
+
+for L in LangFolders:
+
+    with open( f'{abs}/src/fgd/{L}/lp.fgd', 'w' ) as FGD:
+
+        # -Mikk BSPGuy v5 release.
+        #FGD.write( '@mapsize( -4096, 4096 )\n\n' )
+
+        JsonLists = os.listdir( os.path.abspath( f'{abs}/src/fgd/{L}/EntityData/' ) )
+
+        ReadData( os.path.abspath( f'{abs}/src/fgd/{L}/EntityData/BaseClass.json' ) )
+
+        for r in JsonLists:
+
+            fulldir = os.path.join( f'{abs}/src/fgd/{L}/EntityData/', r )
+
+            if fulldir.endswith( '.json' ) and not fulldir.endswith( 'BaseClass.json' ):
+                print( f'{r}')
+                ReadData( fulldir )
+        FGD.close()
